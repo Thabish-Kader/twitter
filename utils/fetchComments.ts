bb254a24fd193a571719edf63a36a428741d393d
Thabish <kadertabish2@gmail.com>
2022-11-13 11:53:56 +0400

    bug fix 3

--- utils/fetchComments.ts
+++ utils/fetchComments.ts
@@ -2,7 +2,7 @@ import { Comment } from "../typings";
 
 export const fetchComments = async (tweetId: string) => {
 	const res = await fetch(
-		`${process.env.NEXT_PUBLIC_BASE_URL}/api/getComments?tweetId=${tweetId}`
+		`https://twitter-nine-ochre.vercel.app/api/getComments?tweetId=${tweetId}`
 	);
 	const data = await res.json();
 	const comments: Comment[] = data.comments;
--- utils/fetchTweets.ts
+++ utils/fetchTweets.ts
@@ -2,7 +2,7 @@ import { Tweet } from "../typings";
 
 export const fetchTweets = async () => {
 	const res = await fetch(
-		`${process.env.NEXT_PUBLIC_BASE_URL}/api/getTweets`
+		`https://twitter-nine-ochre.vercel.app/api/getTweets`
 	);
 	const data = await res.json();
 	const tweets: Tweet[] = data.tweets;
