c84c4402f28e54bbddc9cd68af3a2cfcb84c2a6f
Thabish <kadertabish2@gmail.com>
2022-11-13 12:25:07 +0400

    bug

--- utils/fetchComments.ts
+++ utils/fetchComments.ts
@@ -2,7 +2,7 @@ import { Comment } from "../typings";
 
 export const fetchComments = async (tweetId: string) => {
 	const res = await fetch(
-		`${process.env.NEXT_PUBLIC_BASE_URL}/api/getComments?tweetId=${tweetId}`
+		`${process.env.NEXT_PUBLIC_BASE_URL}api/getComments?tweetId=${tweetId}`
 	);
 	const data = await res.json();
 	const comments: Comment[] = data.comments;
--- utils/fetchTweets.ts
+++ utils/fetchTweets.ts
@@ -1,9 +1,7 @@
 import { Tweet } from "../typings";
 
 export const fetchTweets = async () => {
-	const res = await fetch(
-		`${process.env.NEXT_PUBLIC_BASE_URL}/api/getTweets`
-	);
+	const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}api/getTweets`);
 	const data = await res.json();
 	const tweets: Tweet[] = data.tweets;
 
